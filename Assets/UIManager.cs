75f7047aff07bbe3b546a8542489c9bd150ce90e
HTL2910 <hathanhlong2910@gmail.com>
2024-05-16 10:14:30 +0700

    add asset

---
+++ Assets/UIManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.EventSystems;
+
+public class UIManager : MonoBehaviour ,IPointerEnterHandler
+{
+    // Tìm và lưu trữ tất cả các đối tượng UI Text trong game
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("UI Name: " + gameObject.name);
+    }
+}
