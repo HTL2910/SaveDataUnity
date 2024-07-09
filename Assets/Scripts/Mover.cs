c6f2915eff96e2c29a828e8535a3247d83e46882
HTL2910 <hathanhlong2910@gmail.com>
2024-07-09 14:02:47 +0700

    add  model player, add camera follow player

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -7,8 +7,32 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
 
+    //Ray lastRay;
+    //private void Update() code move player to target and draw ray from camera to mouse position
+    //{
+    //    GetComponent<NavMeshAgent>().destination = target.position;
+    //    if(Input.GetMouseButtonDown(0))
+    //    {
+    //        lastRay=Camera.main.ScreenPointToRay(Input.mousePosition);
+    //    }    
+    //    Debug.DrawRay(lastRay.origin, lastRay.direction*100);
+    //}
+
     private void Update()
     {
-        GetComponent<NavMeshAgent>().destination = target.position;
+        if(Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }    
     }
+    private void MoveToCursor()
+    {
+        Ray ray=Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hashit=Physics.Raycast(ray, out hit);
+        if(hashit)
+        {
+            GetComponent<NavMeshAgent>().destination=hit.point;
+        }
+    }    
 }
