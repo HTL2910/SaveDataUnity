bba761edc957b66c794194d31a4c5141d77f45e4
HTL2910 <hathanhlong2910@gmail.com>
2024-08-22 23:05:34 +0700

    update Cancelling Combat With Movement

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -1,3 +1,4 @@
+using RPG.Combat;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -27,7 +28,11 @@ namespace RPG.Movement
 
             UpdateAnimator();
         }
-
+        public void StartMoveAction(Vector3 destination)
+        {
+            GetComponent<Fighter>().Cancel();
+            MoveTo(destination);
+        }
         public void MoveTo(Vector3 destination)
         {
             navMeshAgent.destination = destination;
