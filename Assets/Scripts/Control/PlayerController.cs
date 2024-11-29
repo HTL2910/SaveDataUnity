db320423e02f1ca3001e25a8bd7212581755ecd2
HTL2910 <hathanhlong2910@gmail.com>
2024-11-29 15:50:19 +0700

    update Die, CanAttack animation ,nested prefab in enemy

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -3,6 +3,7 @@ using RPG.Movement;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 namespace RPG.Control
 {
@@ -21,7 +22,10 @@ namespace RPG.Control
             foreach(RaycastHit hit in hits)
             {
                 CombatTarget target=hit.transform.GetComponent<CombatTarget>();
-                if(target == null) { continue; }
+                if (!GetComponent<Fighter>().CanAttack(target))
+                {
+                    continue;
+                }
                 if (Input.GetMouseButton(0))
                 {
                     GetComponent<Fighter>().Attack(target);
