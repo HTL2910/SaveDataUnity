363be7dfc49a10818e3941631506eeefc6ee5fe5
HTL2910 <hathanhlong2910@gmail.com>
2024-12-01 17:31:26 +0700

    update AIcontroller

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -1,5 +1,6 @@
 using RPG.Combat;
 using RPG.Core;
+using RPG.Movement;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,27 +10,54 @@ namespace RPG.Control
     public class AIController : MonoBehaviour
     {
         [SerializeField] float chaseDistance=5f;
+        [SerializeField] float supicionTime=3f;
         Fighter fighter;
         GameObject player;
         Health health;
+        Mover mover;
+        Vector3 guardPosition;
+        float timeSinceLastSawPlayer=Mathf.Infinity;
         private void Start()
         {
             player = GameObject.FindWithTag("Player");
 
             fighter = GetComponent<Fighter>();
             health = GetComponent<Health>();
+            mover = GetComponent<Mover>();
+            guardPosition=transform.position;
         }
         private void Update()
         {
             if (health.IsDead()) return;
             if (InRangeOfPlayer()  && fighter.CanAttack(player))
             {
-                fighter.Attack(player);
+                timeSinceLastSawPlayer = 0;
+                AttackBehaviour();
+            }
+            else if(timeSinceLastSawPlayer< supicionTime)
+            {
+                SupicionBehaviour();
             }
             else
             {
-                fighter.Cancel();
+                GuardBehaviour();
             }
+            timeSinceLastSawPlayer += Time.deltaTime;
+        }
+
+        private void GuardBehaviour()
+        {
+            mover.StartMoveAction(guardPosition);
+        }
+
+        private void SupicionBehaviour()
+        {
+            GetComponent<ActionScheduler>().CancelCurrentAction();
+        }
+
+        private void AttackBehaviour()
+        {
+            fighter.Attack(player);
         }
 
         private bool InRangeOfPlayer()
